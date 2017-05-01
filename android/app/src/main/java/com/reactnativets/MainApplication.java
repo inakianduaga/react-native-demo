70f0b576ab99c20d1cf7aa708a6c89b4378161d4
Inaki Anduaga <inaki.anduaga@scout24.com>
2017-05-02 00:15:56 +0200

    Fixed codepush app launch

--- android/app/src/main/java/com/reactnativets/MainApplication.java
+++ android/app/src/main/java/com/reactnativets/MainApplication.java
@@ -23,13 +23,6 @@ public class MainApplication extends Application implements ReactApplication {
       return BuildConfig.DEBUG;
     }
 
-    // Override the getJSBundleFile method in order to let the CodePush runtime determine where to
-    // get the JS bundle location from on each app start
-    @Override
-    protected String getJSBundleFile() {
-      return CodePush.getJSBundleFile();
-    }
-
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
