834d49e190d5f0dcb249b9f7615e31e7c6840bd2
Inaki Anduaga <inaki.anduaga@scout24.com>
2017-05-01 22:55:20 +0200

    Added codepush RN JS integration

--- src/Index.tsx
+++ src/Index.tsx
@@ -2,13 +2,19 @@ import React, { Component } from "react";
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore'
 import Main from "./containers/App";
+import codePush from "react-native-code-push";
 
 type IProps = {}
 type IState = {}
 
+const codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, 
+  installMode: codePush.InstallMode.ON_NEXT_RESUME 
+}
+
 const store = configureStore();
 
-export default class App extends Component<IProps, IState> {
+class App extends Component<IProps, IState> {
   render() {
     return (
       <Provider store={ store }>
@@ -17,3 +23,7 @@ export default class App extends Component<IProps, IState> {
     );
   }
 }
+
+const WrappedApp = codePush(codePushOptions)(App);
+
+export default WrappedApp;
