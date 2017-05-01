4b063c5319bf152e0667fd0fe13f4734f6f93ffe
Inaki Anduaga <inaki.anduaga@scout24.com>
2017-05-01 23:51:15 +0200

    Added codepush RN Android integration, app crashing on launch

--- android/app/src/main/java/com/reactnativets/MainApplication.java
+++ android/app/src/main/java/com/reactnativets/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -22,11 +23,27 @@ public class MainApplication extends Application implements ReactApplication {
       return BuildConfig.DEBUG;
     }
 
+    // Override the getJSBundleFile method in order to let the CodePush runtime determine where to
+    // get the JS bundle location from on each app start
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
+
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new NavigationUpdatePackage()
+        new MainReactPackage(),
+        new NavigationUpdatePackage(),
+        // https://microsoft.github.io/code-push/docs/react-native.html#link-5
+        // Instantiate an instance of the CodePush runtime and add it to the list of
+        // existing packages, specifying the right deployment key. If you don't already
+        // have it, you can run "code-push deployment ls <appName> -k" to retrieve your key.
+        new CodePush(
+                getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey),
+                getApplicationContext(),
+                BuildConfig.DEBUG
+        )
       );
     }
   };
