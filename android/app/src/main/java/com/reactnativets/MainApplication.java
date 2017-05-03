b4cc3e876053b27512f21cb87d2b3c360c686377
Inaki Anduaga <inaki.anduaga@scout24.com>
2017-05-03 23:21:44 +0200

    Disabled codepush integration, android build working again

--- android/app/src/main/java/com/reactnativets/MainApplication.java
+++ android/app/src/main/java/com/reactnativets/MainApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.microsoft.codepush.react.CodePush;
+//import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,16 +27,16 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-        new NavigationUpdatePackage(),
+        new NavigationUpdatePackage()//,
         // https://microsoft.github.io/code-push/docs/react-native.html#link-5
         // Instantiate an instance of the CodePush runtime and add it to the list of
         // existing packages, specifying the right deployment key. If you don't already
         // have it, you can run "code-push deployment ls <appName> -k" to retrieve your key.
-        new CodePush(
-                getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey),
-                getApplicationContext(),
-                BuildConfig.DEBUG
-        )
+//        new CodePush(
+//                getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey),
+//                getApplicationContext(),
+//                BuildConfig.DEBUG
+//        )
       );
     }
   };
--- src/Index.tsx
+++ src/Index.tsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { Platform } from 'react-native'
+// import { Platform } from 'react-native'
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore'
 import Main from "./containers/App";
-import codePush from "react-native-code-push";
+// import codePush from "react-native-code-push";
 
 type IProps = {}
 type IState = {}
@@ -20,14 +20,15 @@ class App extends Component<IProps, IState> {
   }
 }
 
+export default App
 // TODO: Unwrap once we have implemented codepush for iOS backend
-let WrappedApp;
-if(Platform.OS === 'android') {
-  WrappedApp = codePush({
-    checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, 
-    installMode: codePush.InstallMode.ON_NEXT_RESUME 
-  })(App);
-}
+// let WrappedApp;
+// if(Platform.OS === 'android') {
+//   WrappedApp = codePush({
+//     checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, 
+//     installMode: codePush.InstallMode.ON_NEXT_RESUME 
+//   })(App);
+// }
 
-// For Android, we deploy CodePush wrap, iOS standard (until implemented)
-export default Platform.OS === 'ios' ? App : WrappedApp;
+// // For Android, we deploy CodePush wrap, iOS standard (until implemented)
+// export default Platform.OS === 'ios' ? App : WrappedApp;
