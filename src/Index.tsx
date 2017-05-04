229c6668c9021adf89e2d1224e2fb1698aac43e3
Inaki Anduaga <inaki.anduaga@scout24.com>
2017-05-04 09:59:20 +0200

    Reenabled codepush functionality on android

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-// import { Platform } from 'react-native'
+import { Platform } from 'react-native'
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore'
 import Main from "./containers/App";
 import { INavigationState } from './reducers/navigation';
-// import codePush from "react-native-code-push";
+import codePush from "react-native-code-push";
 
 type IProps = {
   /* Android will always pass a navigation prop, but iOS might not */
@@ -26,15 +26,15 @@ class App extends Component<IProps, IState> {
   }
 }
 
-export default App
+// export default App
 // TODO: Unwrap once we have implemented codepush for iOS backend
-// let WrappedApp;
-// if(Platform.OS === 'android') {
-//   WrappedApp = codePush({
-//     checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, 
-//     installMode: codePush.InstallMode.ON_NEXT_RESUME 
-//   })(App);
-// }
+let WrappedApp;
+if(Platform.OS === 'android') {
+  WrappedApp = codePush({
+    checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, 
+    installMode: codePush.InstallMode.ON_NEXT_RESUME 
+  })(App);
+}
 
 // // For Android, we deploy CodePush wrap, iOS standard (until implemented)
-// export default Platform.OS === 'ios' ? App : WrappedApp;
+export default Platform.OS === 'ios' ? App : WrappedApp;
